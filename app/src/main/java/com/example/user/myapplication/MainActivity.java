44d4e2985bdab800ad20946b586be59826bf9822
Roee Skandrany <roeeska@gmail.com>
2020-03-14 21:15:36 +0200

    Did Homework

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     public final static String PREF_KEY = "KEY";
     public final static String PREF_NAME = "SETTINGS";
+    public final static String PREF_BC_KEY = "KEY";
+    public final static String PREF_BC = "SETTINGS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +21,38 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
 
 
-        SharedPreferences sp;
-        SharedPreferences.Editor editor;
+        SharedPreferences sp, bc;
+        final SharedPreferences.Editor editor, editorBC;
         sp = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        bc = context.getSharedPreferences(PREF_BC, Context.MODE_PRIVATE);
         editor = sp.edit();
         editor.putString(PREF_KEY,"Hello");
         editor.commit();
+        editorBC = bc.edit();
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editorBC.putString(PREF_BC,"Red");
+                editorBC.commit();
+            }
+        });
+        Button btn4 = (Button) findViewById(R.id.button4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editorBC.putString(PREF_BC,"Green");
+                editorBC.commit();
+            }
+        });
+        Button btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editorBC.putString(PREF_BC,"Blue");
+                editorBC.commit();
+            }
+        });
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/user/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/user/myapplication/SecondActivity.java
@@ -2,8 +2,10 @@ package com.example.user.myapplication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -13,10 +15,21 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Context context = getApplicationContext();
-        SharedPreferences sp;
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_second);
+        SharedPreferences sp, bc;
         SharedPreferences.Editor editor;
         sp = context.getSharedPreferences(MainActivity.PREF_NAME, Context.MODE_PRIVATE);
+        bc = context.getSharedPreferences(MainActivity.PREF_BC, Context.MODE_PRIVATE);
         TextView textView = (TextView) findViewById(R.id.tv_settings);
         textView.setText(sp.getString(MainActivity.PREF_KEY, "Not found"));
+        TextView textView2 = (TextView) findViewById(R.id.textView3);
+        textView2.setText(sp.getString(MainActivity.PREF_BC, "Not found"));
+        if (textView2.getText() == "Red"){
+            rl.setBackgroundColor(Color.RED);
+        }else if(textView2.getText() == "Green"){
+            rl.setBackgroundColor(Color.GREEN);
+        }else if(textView2.getText() == "Blue"){
+            rl.setBackgroundColor(Color.BLUE);
+        }
     }
 }
